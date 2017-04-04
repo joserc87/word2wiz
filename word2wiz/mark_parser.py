f60c470d9187d67324a5afc4738fd4550f5ecbb4
Jose Ramon Cano Yribarren <jose.cano@theconsultancyfirm.com>
2017-04-04 11:42:47 +0200

    Move parse_mark method to utility file

--- word2wiz/mark_parser.py
+++ word2wiz/mark_parser.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 import re
 from .spell_helper import Step
+from .util import parse_mark
 
 DEFAULT_STEP_NAME = 'doc_name'
 DEFAULT_STEP_GROUP_NAME = 'Buitenland'
@@ -52,25 +53,3 @@ def get_steps(marks):
             # If it's not a step mark,it must be a control
             steps[-1].add_control(mark)
     return steps
-
-
-def parse_mark(mark, tag, separator=None):
-    """
-    Returns a Step representing the mark, or null if it's not a Step mark.
-    Args:
-        mark (str): The mark to parse, e.g. 'step myStep'
-        tag (str): The expected begining of th emark, e.g. 'step'
-        separator (str): The separator, e.g. ';', or None if there is none
-    Returns:
-        The rest of the mark stripped, or null if the tag didn't match
-    """
-    if mark.startswith(tag + ' '):
-        mark = mark[len(tag):]
-        if separator is None:
-            return mark.strip()
-        else:
-            parts = mark.split(separator)
-            return [part.strip() for part in parts]
-    else:
-        # If it does not match, return none
-        return None
