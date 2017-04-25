7f12071812e66a6bb619ba4e67d314c45c0aa9ee
Jose Ramon Cano Yribarren <jose.cano@theconsultancyfirm.com>
2017-04-25 17:03:47 +0200

    Fix setuptools to upload package to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,55 @@
 from setuptools import setup
+from codecs import open
+from os import path
 
-setup(name='word2wiz',
-      version='0.2',
-      description='A tool to create wizard configurations from word documents',
-      url='http://github.com/joserc87/word2wiz',
-      author='Jose Ramon Cano',
-      author_email='jose.cano@theconsultancyfirm.com',
-      license='MIT',
-      packages=['word2wiz'],
-      scripts=['bin/word2wiz'],
-      zip_safe=False)
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='word2wiz',
+
+    version='0.2.0',
+
+    description='A tool to create wizard configurations from MS word documents',
+    long_description=long_description,
+
+    url='http://github.com/joserc87/word2wiz',
+
+    author='Jose Ramon Cano',
+    author_email='jose.cano@theconsultancyfirm.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='word2wiz document-wizard docwiz spell',
+
+    packages=['word2wiz'],
+
+    install_requires=['Flask', 'Jinja2', 'python-docx'],
+
+    extras_require={
+        'test': ['behave'],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'word2wiz=word2wiz:main',
+        ],
+    },
+)
