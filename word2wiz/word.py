ba8918b0e16c7a262b2e166aabdbfdb59b665e73
Jose Ramon Cano Yribarren <jose.cano@theconsultancyfirm.com>
2017-05-01 12:16:11 +0200

    Analyse the word document in a generic way
    Now, the marks that delimit the questions in the word document (e.g. <<,
    «, <<, ») are defined in an array that can be extended with other marks.
    The regex used to parse the word document will be constructed based on
    that array.

--- word2wiz/word.py
+++ word2wiz/word.py
@@ -7,7 +7,18 @@ from docx import Document
 def analyse_doc(docx_file):
     document = Document(docx_file)
 
+    marks = [
+        ('«', '»'),
+        ('<<', '>>')
+    ]
+
+    # Regex parts for the delimiters
+    start_dels, end_dels = ('(?:{0})'.format('|'.join(m[i] for m in marks))
+                            for i in [0, 1])
+    # Regex to find the questions
+    regex = '{0}([^{1}]*){1}'.format(start_dels, end_dels)
+
     questions = []
     for p in document.paragraphs:
-        questions += re.findall('(?:«|<<)([^(>>|»)]*)(?:»|>>)', p.text)
+        questions += re.findall(regex, p.text)
     return questions
